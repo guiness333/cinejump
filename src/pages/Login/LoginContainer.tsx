b15f932dc3f0bc036a8623fd85c031d9abac1b0b
Walber Martins <walber123k@hotmail.com>
2020-08-26 15:30:26 -0300

    removendo linha inutil

--- src/pages/Login/LoginContainer.tsx
+++ src/pages/Login/LoginContainer.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled('div')<{width: String}>`
-    display: inline-block;
     width: ${props => String(props.width)};
     height: 1000px;
     position: relative;
