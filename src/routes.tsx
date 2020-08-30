ee473686403318cb1b631e84edf40d216f42aef3
Walber Martins <walber123k@hotmail.com>
2020-08-30 18:12:37 -0300

    Arrumando o nome das pastas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Main from "./pages/Main/main";
+import Main from "./pages/Login/login";
 import Signup from "./pages/Signup/signup";
 
 const Routes = () => (
