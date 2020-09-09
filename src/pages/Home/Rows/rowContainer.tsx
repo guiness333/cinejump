ca3e6b5ad135a77b7575b8320ef1b32723a3cdad
Walber Martins <walber123k@hotmail.com>
2020-09-09 11:23:19 -0300

    Lista de filmes adicionada

--- src/pages/Home/Rows/rowContainer.tsx
+++ src/pages/Home/Rows/rowContainer.tsx
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 
-export const MoviesRow = styled("div")`
+export const MoviesRow = styled("div")<{scroll: String}>`
     margin-right: auto;
     margin-left: auto;
     width: 1010px;
-    float: center;
+    overflow: scroll;
+    overflow-y: hidden;
+    overflow-x: ${(props) => String(props.scroll)};
+    display: flex;
+
 `;
 export const MoviesColumn = styled("div")`
     display: inline-block;
 `;
-export const MovieItem = styled("img")<{width: Number; height: Number; align: String}>`
+export const MovieItem = styled("img")<{width: Number; height: Number; align: String; Title?: String}>`
     display: ${(props) => String(props.align)};
     width: ${(props) => Number(props.width)}px;
     height: ${(props) => Number(props.height)}px;
