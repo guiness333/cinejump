3adfe9ed23b1eed8f13fac6320f63587600300a0
Walber Martins <walber123k@hotmail.com>
2020-09-13 15:46:31 -0300

    Imagem de favoritos adicionada ao filme

--- src/pages/Home/MovieList/movieRow.tsx
+++ src/pages/Home/MovieList/movieRow.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 import { MoviesRow, MovieItem } from "../Rows/rowContainer";
 import { MovieContainer } from "./movieList";
-
+import {Favorito} from '../../Components/Favorito';
+import BlackHeart from '../../../assets/BsHeartFill-black.svg';
 const MovieRow = (props: any) => {
   return (
     <MoviesRow scroll={"none"}>
       {props.movies.map((movie: any) => {
           return (
             <MovieContainer key={movie.id}>
+            <Favorito src={BlackHeart}/>
             <MovieItem
               src={String("https://image.tmdb.org/t/p/w1280/"+movie.poster_path)}
               width={140}
