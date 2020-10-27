35d02b43f5060068a1aaf45dba60859ad4bd823e
Walber Martins <walber123k@hotmail.com>
2020-10-27 13:21:55 -0300

    WJ20-Register -Usuario sendo registrado -Usuario sendo redirecionado para a tela home apos o cadastro

--- src/Components/SignupForm/index.tsx
+++ src/Components/SignupForm/index.tsx
@@ -1,23 +1,79 @@
-import React from 'react';
-import {FormStyle, FiUserS, FiMailS, FiLockS, ButtonStyle, InputFormStyle} from './styles';
+import React, { useState } from "react";
+//import hash from '../../utils';
+import {
+  FormStyle,
+  FiUserS,
+  FiMailS,
+  FiLockS,
+  ButtonStyle,
+  InputFormStyle,
+} from "./styles";
+import Response from "./dtos/Response";
+import loginAPI from "../../services/loginAPI";
+import { Error } from "../Error";
+import { useHistory } from "react-router-dom";
+
 export const SignupForm = () => {
-    return (
-      <FormStyle onSubmit={() => alert("Criar conta")}>
-        <FiUserS />
-        <InputFormStyle placeholder="Nome" type="text"></InputFormStyle>
-        <FiMailS />
-        <InputFormStyle placeholder="E-mail" type="text"></InputFormStyle>
-        <FiLockS />
-        <InputFormStyle placeholder="Senha" type="password"></InputFormStyle>
-        <ButtonStyle
-          type="submit"
-          backgroundColor="#E83F5B"
-          textColor="white"
-          borderColor="#E83F5B"
-          margem={18}
-        >
-          ENTRAR
-        </ButtonStyle>
-      </FormStyle>
-    );
-  };
+  let history = useHistory();
+  const [username, setUsername] = useState(String);
+  const [email, setEmail] = useState(String);
+  const [password, setPassword] = useState(String);
+  const [error, setError] = useState(String);
+
+  async function register(event: any) {
+    event.preventDefault();
+    try {
+      let response: Response = await loginAPI.post(`/users`, {
+        name: username,
+        email: email,
+        password: password,
+      });
+      if(response){
+        localStorage.setItem("user", JSON.stringify(response.data));
+        history.push("/home");
+      }
+    } catch (err) {
+      setError('Verifique se os dados estão preenchidos corretamente!')
+    }
+  }
+
+  return (
+    <FormStyle onSubmit={register}>
+      {error && <Error text={error} />}
+
+      <FiUserS />
+      <InputFormStyle
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+        name="username"
+        placeholder="Nome"
+        type="text"
+      ></InputFormStyle>
+      <FiMailS />
+      <InputFormStyle
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        name="email"
+        placeholder="E-mail"
+        type="text"
+      ></InputFormStyle>
+      <FiLockS />
+      <InputFormStyle
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        name="password"
+        placeholder="Senha"
+        type="password"
+      ></InputFormStyle>
+      <ButtonStyle
+        type="submit"
+        backgroundColor="#E83F5B"
+        textColor="white"
+        borderColor="#E83F5B"
+        margem={18}
+      >
+        Cadastrar
+      </ButtonStyle>
+    </FormStyle>
+  );
+};
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+/*import bcrypt from 'bcrypt';
+export default function Hash(password, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+        return hash;
+    });
+}*/
