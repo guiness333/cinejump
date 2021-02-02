b86a0afd7c215e64caa2b4e604cc5bd1ac2723a4
Walber Martins <walber123k@hotmail.com>
2021-02-02 10:20:24 -0300

    Release 2.0.0 + Footer links

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -7,11 +7,11 @@ export const Footer = () => {
           <LogoFooterStyle fill={'#FFF'}></LogoFooterStyle>
         </LinkContainer>
         <LinkContainer>
-          <LinkStyle>Desenvolvido por Walber Richard</LinkStyle>
-          <LinkStyle>Proposta do projeto</LinkStyle>
-          <LinkStyle>Prototipo no Figma</LinkStyle>
-          <LinkStyle>Apresentação ao comitê</LinkStyle>
-          <LinkStyle>Documentação</LinkStyle>
+          <LinkStyle href="https://github.com/guiness333">Desenvolvido por Walber Richard</LinkStyle>
+          <LinkStyle href="https://classroom.google.com/c/MTQyNzM0ODMyOTE1/m/MTYwMTAyNTUyMjc3/details">Proposta do projeto</LinkStyle>
+          <LinkStyle href="https://www.figma.com/file/um4dcEJCOlEvB6kCe9KCOD/Cinejump?node-id=0%3A1">Prototipo no Figma</LinkStyle>
+          <LinkStyle href="https://docs.google.com/presentation/d/1P8Qv29PMoa56701fNH5g-w9z1vjFtOLaiLPqJwXTNt0/edit?usp=sharing">Apresentação ao comitê</LinkStyle>
+          <LinkStyle href="https://github.com/guiness333/cinejump">Documentação</LinkStyle>
         </LinkContainer>
       </FooterStyle>
     );
